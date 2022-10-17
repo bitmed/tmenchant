5c9347db3cff0787e5a6a3fb723cc289644f07d7
Matteo Molinaro <m.molinaro@bitgrapes.com>
2022-10-17 16:51:39 +0200

    Added bard runes

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ const majorRunes = [
   { label: 'Male', type: 0, blood: ['demoniaco'] },
   { label: 'Danno', type: 1, blood: ['vigoroso'] },
   { label: 'Precisione', type: 1, blood: ['volatile'] },
-  { label: 'Parassitico', type: 0, blood: ['corrotto'] },
+  { label: 'Parassitico', type: 1, blood: ['corrotto'] },
   { label: 'Dispersione', type: 0, blood: ['etereo'] },
   { label: 'Canalizzante', type: 0, blood: ['incandescente', 'glaciale', 'titanico', 'etereo'] },
   { label: 'Sterminatore', type: 0, blood: ['vigoroso','volatile','stabile'] },
   { label: 'Scioglilingua', type: 1, blood: ['volatile'] },
   { label: 'Indissolubile', type: 1, blood: ['stabile'] },
+  { label: 'Conciliante', type: 2, blood: ['stabile'] },
+  { label: 'Provocante', type: 2, blood: ['vigoroso'] },
+  { label: 'Assordante', type: 2, blood: ['volatile'] },
+  { label: 'Vibrante', type: 2, blood: ['titanico'] },
 ];
 
 const levelMultipliers = [
@@ -30,11 +34,18 @@ const levelMultipliers = [
   { crystal: 'cristalli', quantity: 2, core: 1, superiorCore: 0 },
   { crystal: 'cristalli', quantity: 4, core: 0, superiorCore: 1 },
 ];
+const pureMultipliers = [
+  { crystal: 'polveri', quantity: 7, core: 0, superiorCore: 0 },
+  { crystal: 'frammenti', quantity: 4, core: 0, superiorCore: 0 },
+  { crystal: 'frammenti', quantity: 7, core: 0, superiorCore: 0 },
+  { crystal: 'cristalli', quantity: 3, core: 1, superiorCore: 0 },
+  { crystal: 'cristalli', quantity: 5, core: 0, superiorCore: 1 },
+];
 
 const bloodMultipliers = {
   'polveri': 1,
   'frammenti': 2,
-  'cristalli': 5
+  'cristalli': 6
 };
 
 const minorRunes = [
@@ -48,15 +59,42 @@ function App() {
   const [minors, setMinors] = useState([]);
 
   const availableMajorRunes = (current) => {
-    let type0 = false;
+    let exclusiveType = -1;
     let names = [];
     let currentType = majorRunes.find( item => item.label === current.name ).type;
     for( let i = 0; i < runes.length; i++) {
-      if( majorRunes.find( item => item.label === runes[i].name ).type == 0 ) type0 = true;
+      let t = majorRunes.find( item => item.label === runes[i].name ).type
+      if( t == 0 || t == 2) exclusiveType = t;
       if( runes[i] !== current && runes[i].name !== '' ) names.push(runes[i].name);
     }
 
-    return majorRunes.filter( item => (item.type != 0 || !type0 || currentType == 0 ) && names.indexOf(item.label) == -1 );
+    return majorRunes.filter( item => {
+      console.log(
+        item.label,
+        currentType,
+        item.type,
+        exclusiveType,
+        item.label == '',
+        exclusiveType == -1,
+        (item.type == 2 && exclusiveType == 2), 
+        (exclusiveType == 0 && item.type == 1),
+        currentType == 0,
+        currentType == 2,'RES', (item.label == '' 
+        || exclusiveType == -1 
+        || (item.type == 2 && exclusiveType == 2) 
+        || (exclusiveType == 0 && item.type == 1)
+        || currentType == 0
+        || currentType == 2 ) && names.indexOf(item.label) == -1
+      );
+      return (item.label == ''
+        || names.length == 0
+        || (exclusiveType == -1 && item.type != 2)
+        || (item.type == 2 && exclusiveType == 2) 
+        || (exclusiveType == 0 && item.type == 1)
+        || currentType == 0
+        || currentType == 2 ) && names.indexOf(item.label) == -1
+      });
+    //return majorRunes.filter( item => (item.type != 0 || !type0 || currentType == 0 ) && names.indexOf(item.label) == -1 );
   }
 
   const availableMinorRunes = (current) => {
@@ -73,15 +111,17 @@ function App() {
     let costs = { 'polveri': 0, 'frammenti': 0, 'cristalli': 0, 'Nucleo': 0, 'Nucleo Formidabile': 0 };
     let runeData = majorRunes.find(item => item.label === rune.name);
 
-    for( let i = 0; i < rune.level; i++) {
+    for( let i = rune.from; i < rune.level; i++) {
       let cost = levelMultipliers[i];
+      if( rune.name === 'Magico')
+        cost = pureMultipliers[i];
 
       //if( costs.hasOwnProperty(cost.crystal) ) costs[cost.crystal] += cost.quantity;
       //else costs[cost.crystal] = cost.quantity;
       costs[cost.crystal] += cost.quantity * (runeData.blood.length > 0 ? runeData.blood.length : 1);
 
-      if( cost.nucleus ) costs['Nucleo'] = 1;
-      if( cost.superiorNucleus ) costs['Nucleo Formidabile'] = 1;
+      if( cost.core ) costs['Nucleo'] = 1;
+      if( cost.superiorCore ) costs['Nucleo Formidabile'] = 1;
 
       runeData.blood.forEach( blood => {
         if( costs.hasOwnProperty(blood) ) costs[blood] += cost.quantity * bloodMultipliers[cost.crystal];
@@ -95,21 +135,31 @@ function App() {
   const minorMaterialCost = (rune) => {
     if( rune.name === '') return {};
     let costs = { 'gold': 0 };
-    for( let i = 1; i <= rune.level; i++ )
-      costs['gold'] += i * 50000;
+    for( let i = rune.from; i < rune.level; i++ )
+      costs['gold'] += (i+1) * 50000;
     return costs;
   }
 
   const updateRune = (rune, newName) => {
-    updateRuneLevel(rune, newName === '' ? 1 : rune.level, newName);
+    updateRuneLevel(rune, newName === '' ? 0 : rune.from, newName === '' ? 1 : rune.level, newName);
   }
 
-  const updateRuneLevel = (rune, value, name = false) => {
+  const updateRuneFrom = (rune, value) => {
+    let to = rune.level;
+    value = parseInt(value);
+    if( value >= to) to = value+1;
+    if(to > 5) to = 5;
+    updateRuneLevel(rune, value, to);
+  }
+
+  const updateRuneLevel = (rune, from, value, name = false) => {
     let newList = [];
     let totalLevels = 0;
+    if(from > value) from = value;
     value = parseInt(value);
     for(let i = 0; i < runes.length; i++) {
       if(runes[i] === rune) {
+        runes[i].from = parseInt(from);
         runes[i].level = parseInt(value);
         if(name !== false) runes[i].name = name;
       }
@@ -123,13 +173,23 @@ function App() {
   }
 
   const updateMinorRune = (rune, newName) => {
-    updateMinorRuneLevel(rune, newName === '' ? 1 : rune.level, newName);
+    updateMinorRuneLevel(rune, newName === '' ? 0 : rune.from, newName === '' ? 1 : rune.level, newName);
   }
 
-  const updateMinorRuneLevel = (rune, value, name = false) => {
+  const updateMinorRuneFrom = (rune, value) => {
+    let to = rune.level;
+    value = parseInt(value);
+    if( value >= to) to = value;
+    if(to > 5) to = 5;
+    updateMinorRuneLevel(rune, value, to);
+  }
+
+  const updateMinorRuneLevel = (rune, from, value, name = false) => {
     let newList = [...minors];
+    if(from > value) from = value;
     for( let i = 0; i < newList.length; i++) {
       if( newList[i] === rune ) {
+        newList[i].from = parseInt(from);
         newList[i].level = parseInt(value);
         if(name !== false) newList[i].name = name;
       }
@@ -139,7 +199,7 @@ function App() {
 
   const addRune = () => {
     let newList = [...runes];
-    newList.push({ name:'', level: 1 });
+    newList.push({ name:'', level: 1, from: 0 });
     setRunes(newList);
   }
 
@@ -155,7 +215,7 @@ function App() {
 
   const addMinorRune = () => {
     let newList = [...minors];
-    newList.push({ name: '', level: 1 });
+    newList.push({ name: '', level: 1, from: 0 });
     setMinors(newList);
   }
 
@@ -177,7 +237,7 @@ function App() {
   let totalCost = {};
   for( let i = 0; i < runes.length; i++) {
     let runeCost = majorMaterialCost(runes[i]);
-    runesRendered.push(<Rune rune={runes[i]} availableLevels={availableLevels} removeRune={removeRune} availableRunes={availableMajorRunes(runes[i])} changeRune={updateRune} changeRuneLevel={updateRuneLevel} cost={runeCost} />);
+    runesRendered.push(<Rune rune={runes[i]} availableLevels={availableLevels} removeRune={removeRune} availableRunes={availableMajorRunes(runes[i])} changeRune={updateRune} changeRuneLevel={updateRuneLevel} changeRuneFrom={updateRuneFrom} cost={runeCost} />);
 
     for( const k in runeCost ) {
       if( totalCost.hasOwnProperty(k) ) totalCost[k] += runeCost[k];
@@ -188,7 +248,7 @@ function App() {
   let minorRunesRendered = [];
   for( let i = 0; i < minors.length; i++) {
     let runeCost = minorMaterialCost(minors[i]);
-    minorRunesRendered.push(<Rune rune={minors[i]} availableLevels={9999} removeRune={removeRune} availableRunes={availableMinorRunes(minors[i])} changeRune={updateMinorRune} changeRuneLevel={updateMinorRuneLevel} cost={runeCost} />);
+    minorRunesRendered.push(<Rune rune={minors[i]} availableLevels={9999} removeRune={removeMinorRune} availableRunes={availableMinorRunes(minors[i])} changeRune={updateMinorRune} changeRuneLevel={updateMinorRuneLevel}  changeRuneFrom={updateMinorRuneFrom} cost={runeCost} />);
 
     for( const k in runeCost ) {
       if( totalCost.hasOwnProperty(k) ) totalCost[k] += runeCost[k];
@@ -199,7 +259,7 @@ function App() {
   let totalCostRendered = [];
   for( const k in totalCost ) {
     if(totalCost[k])
-      totalCostRendered.push(<><span>{k}</span><span>{totalCost[k]}</span></>);
+      totalCostRendered.push(<div><span>{k}</span> <span>{totalCost[k]}</span></div>);
   }
 
   return (
@@ -211,12 +271,12 @@ function App() {
         <div className='major-runes'>
           <h2>Rune maggiori</h2>
           {runesRendered}
-          {runes.length < 5 && availableLevels > 0 && <a onClick={addRune}>Aggiungi runa</a>}
+          {runes.length < 6 && availableLevels > 0 && <div className='add-rune-container'><a className='add-rune' onClick={addRune}>Aggiungi runa maggiore</a></div>}
         </div>
         <div className='minor-runes'>
           <h2>Rune minori</h2>
           {minorRunesRendered}
-          {minors.length < minorRunes.length - 1 && <a onClick={addMinorRune}>Aggiungi runa</a>}
+          {minors.length < minorRunes.length - 1 && <div className='add-rune-container'><a className='add-rune' onClick={addMinorRune}>Aggiungi runa minore</a></div>}
         </div>
         <div className='total-cost'>
           <h3>Costo totale</h3>
--- src/components/Rune.js
+++ src/components/Rune.js
@@ -38,32 +38,45 @@ function Rune(props) {
   if( !props.rune ) return (<></>);
 
   let optionLevels = [];
-  for( let i = 1; i <= 5 && i <= props.rune.level + props.availableLevels; i++ )
+  let optionLevelsFrom = [];
+  for( let i = 1; i <= 5 && i <= props.rune.level + props.availableLevels; i++ ) {
+    if( i < 5  && i < props.rune.level + props.availableLevels ) optionLevelsFrom.push(<option>{i}</option>);
     optionLevels.push(<option>{i}</option>);
+  }
 
   let cost = [];
   for(const k in props.cost) {
     if(props.cost[k])
-      cost.push( <><span>{k}</span><span>{props.cost[k]}</span></> );
+      cost.push( <span className='cost-item'><span>{k}</span> <span>{props.cost[k]}</span></span> );
   }
 
   return (
-    <div>
-      <div>
-        <span>Runa</span>
-        <select value={props.rune.name} onChange={ev => props.changeRune(props.rune, ev.target.value)}>
-        {props.availableRunes.map(item => <option key={item.label} value={item.label}>{item.label}</option>)}
-        </select>
-        {props.rune.name !== '' && <>
+    <div className='rune-row'>
+      <div className='rune-data'>
+        <div>
+          <span>Runa</span>
+          <select className='rune-name' value={props.rune.name} onChange={ev => props.changeRune(props.rune, ev.target.value)}>
+          {props.availableRunes.map(item => <option key={item.label} value={item.label}>{item.label}</option>)}
+          </select>
+        </div>
+        {props.rune.name !== '' && <div className='rune-level'>
           <span>Livello</span>
-          <select value={props.rune.level} onChange={ev => props.changeRuneLevel(props.rune, ev.target.value)}>
+          <span>da</span>
+          <select value={props.rune.from} onChange={ev => props.changeRuneFrom(props.rune, ev.target.value, props.rune.level)}>
+            <option>0</option>
+            {optionLevelsFrom}
+          </select>
+          <span>a</span>
+          <select value={props.rune.level} onChange={ev => props.changeRuneLevel(props.rune, props.rune.from, ev.target.value)}>
             {optionLevels}
           </select>
-          </>
+          </div>
         }
       </div>
-      {props.rune.name !== '' && <div className='runeCost'>{cost}</div>}
-      <div>
+      <div className='rune-cost'>
+        {props.rune.name !== '' && <div className='runeCost'>{cost}</div>}
+      </div>
+      <div className='rune-buttons'>
         <a onClick={ev => props.removeRune(props.rune)}>-</a>
       </div>
     </div>
